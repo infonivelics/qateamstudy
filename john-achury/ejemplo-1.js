aeebf1612e18fd8e8e869b34c63cb0581f72cd44
john achury <john.achury@nivelics.com>
2023-01-20 14:06:21 -0500

    inclusion condicional pregunta campoCero

--- john-achury/ejemplo-1.js
+++ john-achury/ejemplo-1.js
@@ -2,15 +2,30 @@ const readline = require('readline-sync');
 
 console.log("Hola Mundo y buenos dias...");
 
-  campoCero = readline.question(`Para iniciar, porfavor digame su nombre `);
+  preguntacampoCero = readline.question(`Desea digitar su nombre? `); 
 
-console.log('hola, '+ campoCero, 'Soy un pequeño algoritmo que hace una suma basica de 2 sumandos y para iniciarla hacer,');
-
-  campoOne = readline.question(`inicie digitando un numero o el sumando #1  `);
-
-  campoTwo = readline.question(`Listo, ahora porfavor, digite el otro numero o el sumando #2  `);
+  if(preguntacampoCero == "SI"){
     
-result = Number(campoOne) + Number(campoTwo);
 
-console.log("Deacuerdo, El resultado de la suma entre estos 2 sumandos ingresados es: " + result, 
-'Hasta pronto!');
+    campoCero = readline.question(`Para iniciar, porfavor digame su nombre `);
+
+    console.log('hola, '+ campoCero, 'Soy un pequeño algoritmo que hace una suma basica de 2 sumandos y para iniciarla hacer,');
+    
+      campoOne = readline.question(`inicie digitando un numero o el sumando #1  `);
+    
+      campoTwo = readline.question(`Listo, ahora porfavor, digite el otro numero o el sumando #2  `);
+        
+    result = Number(campoOne) + Number(campoTwo);
+    
+    console.log("Deacuerdo, El resultado de la suma entre estos 2 sumandos ingresados es: " + result, 
+    'Hasta pronto!');
+
+ 
+  }
+
+  else{
+console.log("el programa a finalizado");
+
+  }
+  
+  
